b2b62cda1f34f9e684b56ffc23aaab99b53840a7
Bhavesh bhide <bhavesh.bhide@gmail.com>
2013-10-16 04:26:28 +0530

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,35 @@
+<?php
+include "inc/header.php";
+if(!isset($_SESSION['username'])) {
+	header('location:login.php');
+}
+?>
+<div class="sixteen columns">
+	<div class="box clearfix">
+		<?php 
+			$task = 'get_all';
+			if(isset($_GET['task'])) {
+				$task = $_GET['task'];
+			}
+			$mysurvey->process($task);
+		?>
+	</div>
+</div>
+	<script type="text/javascript" src="./js/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="./js/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript">
+		jQuery(function ($) {
+			$('.add_more').click(function(e){
+				e.preventDefault();
+				$('.options').append('<li><input name="options[]" type="text"></li>');
+				return false;
+			});
+
+			$('.remove').click(function(e){
+				e.preventDefault();
+				$(this).parents('li').remove();
+				return false;
+			})
+		})
+	</script>
+<?php include "inc/footer.php"; ?>
